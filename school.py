379009e8639dfa2429db4cb1e15f324694fa0f2b
GhostWalker777 <48738681+GhostWalker777@users.noreply.github.com>
2020-03-01 20:03:12 -0500

    Add files via upload

---
+++ school.py
@@ -0,0 +1,75 @@
+char = {'name':'Hero',
+        'lvl': 1,
+        'xp': 0,
+        'lvlNext': 25,
+        'stats':{'str': 1,
+                 'dex': 1,
+                 'int': 1,
+                 'hp': 30,
+                 'atk':[5,12]}}
+
+imp = {'name': 'Imp',
+       'lvl': 1,
+       'xp': 0,
+       'lvlNext': 25,
+       'reward': 25,
+       'stats':{'str': 1,
+                 'dex': 1,
+                 'int': 1,
+                 'hp': 30,
+                 'atk':[5,12]}}
+
+
+def level(char,stats):
+    nStr, nDex, nInt = 0, 0, 0
+    while char['xp'] >= char['lvlNext']:
+        char['lvl'] += 1
+        char['xp'] = char['xp'] - char['lvlNext']
+        char['lvlNext'] = round(char['lvlNext'] * 1.5)
+        nStr += 1
+        nDex += 1
+        nInt += 1
+
+    print('level:', char['lvl']) #level
+    # print('STR +{} DEX +{} INT +{}'.format(nStr, nDex, nInt)) #old stats +new stats
+    # print()
+    #The assignment as I gave it last lesson:
+    print('STR {} +{} DEX {} +{} INT {} +{}'.format(char['stats']['str'], nStr,
+                                                    char['stats']['dex'], nDex,
+                                                    char['stats']['int'], nInt))
+    char['stats']['str'] += nStr
+    char['stats']['dex'] += nDex
+    char['stats']['int'] += nInt
+
+from random import randint
+
+def takeDmg(attacker, defender):
+    dmg = randint(attacker['stats']['atk'][0], attacker['stats']['atk'][1])
+    defender['stats']['hp'] = defender['stats']['hp'] - dmg
+    if defender['stats']['hp'] <= 0:
+        print('{} has been slain'.format(defender['name']))
+        char['xp'] += imp['reward']
+        level(hero)
+        input('Press any key to quit.')
+        exit(0)
+    else:
+        print('{} takes {} damage!'.format(defender['name'], dmg))
+
+def commands(player, enemy):
+    while True:
+        print('------------------')
+        cmd = input('Do you want to attack? yes/no: ').lower()
+        if 'yes' in cmd:
+            takeDmg(player, enemy)
+        elif 'no' in cmd:
+            print('{} takes the opportunity to attack!'.format(enemy['name']))
+        else:
+            pass
+
+char['xp'] += 25
+level(char, char['stats'])
+
+char['xp'] += 500
+level(char, ['stats'])
+
+commands(char,imp)
