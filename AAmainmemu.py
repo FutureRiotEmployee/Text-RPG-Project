c749eed629f1db7707b183e7e7e6008e83c26105
Mvkrl <61705337+Mvkrl@users.noreply.github.com>
2020-03-04 12:41:49 -0500

    Add files via upload

---
+++ AAmainmemu.py
@@ -0,0 +1,24 @@
+#main Membu
+
+play = ' '
+def mainMenu():
+    print ('Welcome to the Eeop 2d adventure.\n')
+    print ('     Play     \n\n     Exit     \n')
+    play = input()
+    if play == ('play'):
+        print("Once upon a age,in a land much different than our own, no one can see but all can read.")
+        print ('You wake up with only awareness, your in a old house.')
+        print('You manage to find a small broken sword, its oxidized and starting to crumble, it should hold up.')
+    elif play == 'exit':
+        print ('>:( ')
+
+    
+        
+
+
+    
+
+
+
+
+mainMenu()
