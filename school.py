20dae5539ded9aad240fad457a7dfba64f6148aa
GhostWalker777 <48738681+GhostWalker777@users.noreply.github.com>
2020-03-05 13:00:51 -0500

    Add files via upload
    Latest Version as of 3/5/2020 1:00PM

--- school.py
+++ school.py
@@ -2,6 +2,8 @@ char = {'name':'Hero',
         'lvl': 1,
         'xp': 0,
         'lvlNext': 25,
+        'potions':{'health': 1,
+                   'strength': 3},
         'stats':{'str': 1,
                  'dex': 1,
                  'int': 1,
@@ -48,21 +50,48 @@ def takeDmg(attacker, defender):
     defender['stats']['hp'] = defender['stats']['hp'] - dmg
     if defender['stats']['hp'] <= 0:
         print('{} has been slain'.format(defender['name']))
-        char['xp'] += imp['reward']
-        level(hero)
+        char['xp'] += imp['reward'] #Find a way to make this interchangable
+        level(char,['stats'])
         input('Press any key to quit.')
-        exit(0)
     else:
         print('{} takes {} damage!'.format(defender['name'], dmg))
 
 def commands(player, enemy):
     while True:
+        if enemy['stats']['hp'] <= 0:
+            break
         print('------------------')
-        cmd = input('Do you want to attack? yes/no: ').lower()
+        cmd = input('Do you want to swing? yes/no: ').lower()
         if 'yes' in cmd:
             takeDmg(player, enemy)
         elif 'no' in cmd:
-            print('{} takes the opportunity to attack!'.format(enemy['name']))
+            while True:
+                print('Do you want to use a potion? or Flee?: potion/flee/back')
+                dec = input('').lower()
+                ##print('{} takes the opportunity to attack!'.format(enemy['name']))
+                if dec == 'potion':
+                    print('You have',char['potions']['health'], 'health potions and',char['potions']['strength'],'strength potions')
+                    pot = input('Which would you like to use? health/strength: ').lower()
+                    if pot == 'health':
+                        if char['potions']['health'] > 0:
+                            print('You drink a health potion and feel your wounds begin to close')
+                            char['potions']['health'] - 1
+                            hpAdd = char['stats']['int']##TODO Changes entire health bar
+                            char['stats']['hp'] = hpAdd + 10
+                            print(char['stats']['hp'])
+                        elif char['potions']['health'] <= 0:
+                            print('You don\'t have any health potions!')
+                            break
+                        else:
+                            print('Error with potion amount values line 80')
+                    break
+                elif dec == 'flee':
+                    print('placeholder')
+                    break
+                elif dec == 'back':
+                    break
+                else:
+                    pass
         else:
             pass
 
