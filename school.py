14db5da1552c9bc021a96a5d216636dd768b8f1e
GhostWalker777 <48738681+GhostWalker777@users.noreply.github.com>
2020-03-06 13:03:07 -0500

    Add files via upload

--- school.py
+++ school.py
@@ -1,52 +1,54 @@
+from random import randint
+#Set a variable for attack, times it by your attack stat then randint it for its total attack
+
 char = {'name':'Hero',
         'lvl': 1,
         'xp': 0,
         'lvlNext': 25,
-        'potions':{'health': 1,
+        'turn':True,
+        'potions':{'health': 2,
                    'strength': 3},
-        'stats':{'str': 1,
+        'stats':{'stre': 1,
                  'dex': 1,
                  'int': 1,
-                 'hp': 30,
-                 'atk':[5,12]}}
+                 'maxhp': 30,
+                 'hp': 10,}}
+
 
 imp = {'name': 'Imp',
        'lvl': 1,
        'xp': 0,
        'lvlNext': 25,
        'reward': 25,
-       'stats':{'str': 1,
-                 'dex': 1,
-                 'int': 1,
-                 'hp': 30,
-                 'atk':[5,12]}}
+       'stats':{'stre': 1,
+                'dex': 1,
+                'int': 1,
+                'hp': 3000,
+                'maxhp':3000,
+                'atk':[5,12]}}
 
 
 def level(char,stats):
-    nStr, nDex, nInt = 0, 0, 0
+    nStre, nDex, nInt = 0, 0, 0
     while char['xp'] >= char['lvlNext']:
         char['lvl'] += 1
         char['xp'] = char['xp'] - char['lvlNext']
         char['lvlNext'] = round(char['lvlNext'] * 1.5)
-        nStr += 1
+        nStre += 1
         nDex += 1
         nInt += 1
 
-    print('level:', char['lvl']) #level
-    # print('STR +{} DEX +{} INT +{}'.format(nStr, nDex, nInt)) #old stats +new stats
-    # print()
-    #The assignment as I gave it last lesson:
-    print('STR {} +{} DEX {} +{} INT {} +{}'.format(char['stats']['str'], nStr,
+    print('level:', char['lvl'])
+    print('STR {} +{} DEX {} +{} INT {} +{}'.format(char['stats']['stre'], nStre,
                                                     char['stats']['dex'], nDex,
                                                     char['stats']['int'], nInt))
-    char['stats']['str'] += nStr
+    char['stats']['stre'] += nStre
     char['stats']['dex'] += nDex
     char['stats']['int'] += nInt
 
-from random import randint
 
 def takeDmg(attacker, defender):
-    dmg = randint(attacker['stats']['atk'][0], attacker['stats']['atk'][1])
+    dmg = randint(1,3) * char['stats']['stre']
     defender['stats']['hp'] = defender['stats']['hp'] - dmg
     if defender['stats']['hp'] <= 0:
         print('{} has been slain'.format(defender['name']))
@@ -55,6 +57,7 @@ def takeDmg(attacker, defender):
         input('Press any key to quit.')
     else:
         print('{} takes {} damage!'.format(defender['name'], dmg))
+        print(defender['stats']['hp'],"/",defender['stats']['maxhp'])
 
 def commands(player, enemy):
     while True:
@@ -75,15 +78,25 @@ def commands(player, enemy):
                     if pot == 'health':
                         if char['potions']['health'] > 0:
                             print('You drink a health potion and feel your wounds begin to close')
-                            char['potions']['health'] - 1
-                            hpAdd = char['stats']['int']##TODO Changes entire health bar
-                            char['stats']['hp'] = hpAdd + 10
+                            char['potions']['health'] = char['potions']['health'] - 1
+                            hpAdd = char['stats']['int']
+                            maxHp = char['stats']['maxhp']
+                            hpTotal = hpAdd + 10
+                            hp = char['stats']['hp']
+                            if maxHp - hp > hpAdd + 10:
+                                char['stats']['hp'] = hp + hpTotal
+                            elif maxHp - hp < hpAdd + 10:
+                                char['stats']['hp'] = maxHp
+                                print('You feel yourself return to your peak state!(No more HP can be gained)')
+                            #char['stats']['hp'] = hpAdd + 10
                             print(char['stats']['hp'])
                         elif char['potions']['health'] <= 0:
                             print('You don\'t have any health potions!')
                             break
                         else:
                             print('Error with potion amount values line 80')
+                    elif pot == 'strength':
+                        print('placeholder')
                     break
                 elif dec == 'flee':
                     print('placeholder')
