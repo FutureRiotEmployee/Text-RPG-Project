1d4b5ee852df4a860796533e25c850f2715d0d81
GhostWalker777 <48738681+GhostWalker777@users.noreply.github.com>
2020-03-10 06:32:21 -0400

    Add files via upload

--- school.py
+++ school.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 #Set a variable for attack, times it by your attack stat then randint it for its total attack
 
 char = {'name':'Hero',
@@ -9,10 +10,11 @@ char = {'name':'Hero',
         'potions':{'health': 2,
                    'strength': 3},
         'stats':{'stre': 1,
+                 'maxStre':1,
                  'dex': 1,
                  'int': 1,
                  'maxhp': 30,
-                 'hp': 10,}}
+                 'hp': 30,}}
 
 
 imp = {'name': 'Imp',
@@ -23,17 +25,18 @@ imp = {'name': 'Imp',
        'stats':{'stre': 1,
                 'dex': 1,
                 'int': 1,
-                'hp': 3000,
-                'maxhp':3000,
-                'atk':[5,12]}}
+                'hp': 60,
+                'maxhp':60,
+                'atk':[5,8]}}
 
 
 def level(char,stats):
-    nStre, nDex, nInt = 0, 0, 0
+    nStre, nDex, nInt, nMaxStre = 0, 0, 0, 0
     while char['xp'] >= char['lvlNext']:
         char['lvl'] += 1
         char['xp'] = char['xp'] - char['lvlNext']
         char['lvlNext'] = round(char['lvlNext'] * 1.5)
+        nMaxStre += 1
         nStre += 1
         nDex += 1
         nInt += 1
@@ -42,29 +45,47 @@ def level(char,stats):
     print('STR {} +{} DEX {} +{} INT {} +{}'.format(char['stats']['stre'], nStre,
                                                     char['stats']['dex'], nDex,
                                                     char['stats']['int'], nInt))
+    char['stats']['maxStre'] += nMaxStre
     char['stats']['stre'] += nStre
     char['stats']['dex'] += nDex
     char['stats']['int'] += nInt
 
 
 def takeDmg(attacker, defender):
-    dmg = randint(1,3) * char['stats']['stre']
-    defender['stats']['hp'] = defender['stats']['hp'] - dmg
-    if defender['stats']['hp'] <= 0:
-        print('{} has been slain'.format(defender['name']))
-        char['xp'] += imp['reward'] #Find a way to make this interchangable
-        level(char,['stats'])
-        input('Press any key to quit.')
-    else:
-        print('{} takes {} damage!'.format(defender['name'], dmg))
-        print(defender['stats']['hp'],"/",defender['stats']['maxhp'])
+    if char['turn'] == True:
+        dmg = randint(1,3) * char['stats']['stre']
+        defender['stats']['hp'] = defender['stats']['hp'] - dmg
+        if defender['stats']['hp'] <= 0:
+            print('{} has been slain'.format(defender['name']))
+            char['stats']['stre'] = char['stats']['maxStre']
+            char['xp'] += defender['reward'] 
+            level(char,['stats'])
+            input('Press any key to quit.')#Perhaps remove??
+        else:
+            print('{} takes {} damage!'.format(defender['name'], dmg))
+            print(defender['stats']['hp'],"/",defender['stats']['maxhp'])
+            char['turn'] = False
+    elif char['turn'] == False:
+        dmgE = randint(1,2) * attacker['stats']['stre']
+        attacker['stats']['hp'] = attacker['stats']['hp'] - dmgE
+        if attacker['stats']['hp'] <= 0:
+            print('Your vision starts to fade to darkness as the {} overpowers you...'.format(defender['name']))
+        else:
+            print('{} takes {} damage!'.format(attacker['name'], dmgE))
+            print(attacker['stats']['hp'],"/",attacker['stats']['maxhp'])
+            char['turn'] = True
 
 def commands(player, enemy):
     while True:
         if enemy['stats']['hp'] <= 0:
             break
         print('------------------')
-        cmd = input('Do you want to swing? yes/no: ').lower()
+        if char['turn'] == False:
+            print('It\'s {}\'s turn!'.format(enemy['name']))
+            sleep(1)
+            takeDmg(player, enemy)
+            #Perhaps add dexterity use here?
+        cmd = input('Do you want to attack? yes/no: ').lower()
         if 'yes' in cmd:
             takeDmg(player, enemy)
         elif 'no' in cmd:
@@ -92,14 +113,21 @@ def commands(player, enemy):
                             print(char['stats']['hp'])
                         elif char['potions']['health'] <= 0:
                             print('You don\'t have any health potions!')
-                            break
+                            
                         else:
                             print('Error with potion amount values line 80')
                     elif pot == 'strength':
-                        print('placeholder')
+                        if char['potions']['strength'] > 0:
+                            print('You feel your muscles tighten past their usual limit')
+                            char['potions']['strength'] = char['potions']['strength'] - 1
+                            char['stats']['stre'] = char['stats']['stre'] + 2
+                            print('Strength ',char['stats']['stre'],'/',char['stats']['maxStre'])
+                        elif char['potions']['health'] <= 0:
+                            print('You don\'t have any health potions!')
+                            
                     break
                 elif dec == 'flee':
-                    print('placeholder')
+                    print('placeholder')#Dextrity use here too?
                     break
                 elif dec == 'back':
                     break
