8740d2af59ba27ad62bd68733e06ab3e8736be33
DABABAYAGA <61705300+DABABAYAGA@users.noreply.github.com>
2020-03-12 12:58:19 -0400

    Add files via upload

---
+++ Items_3sets.py
@@ -0,0 +1,238 @@
+
+item72 = {'name':'Excellent Plate Helmet',
+        'strength value': 5,
+        'dexterity value':8 ,
+        'intel value': 10,
+        'health value':35}
+
+
+item71 = {'name':'Excellent Plate Armor',
+        'strength value': 6,
+        'dexterity value':9 ,
+        'intel value':11 ,
+        'health value':40}
+
+item70 = {'name':'Excellent Mage Hood',
+        'strength value': -8,
+        'dexterity value': 6,
+        'intel value': 35,
+        'health value':9}
+
+item69 = {'name':'Excellent Mage Robes',
+        'strength value': -5,
+        'dexterity value': 7,
+        'intel value': 35,
+        'health value':15}
+
+item68 = {'name':'Excellent Leather Guise',
+        'strength value': 5,
+        'dexterity value': 6,
+        'intel value': 11,
+        'health value':30}
+
+item67 = {'name':'Excellent Leather Armor',
+        'strength value': 6,
+        'dexterity value': 7,
+        'intel value': 12,
+        'health value':35}
+
+item66 = {'name':'Excellent Dagger',
+        'strength value': 6,
+        'dexterity value': 22,
+        'intel value': 9,
+        'health value':-6}
+
+item65 = {'name':'Excellent Amplifying Tome',
+        'strength value': -5,
+        'dexterity value': 5,
+        'intel value': 13,
+        'health value':0}
+
+item64 = {'name':'Excellent Staff',
+        'strength value': 6,
+        'dexterity value': 10,
+        'intel value': 27,
+        'health value':0}
+
+item63 = {'name':'Excellent Long Sword',
+        'strength value': 30,
+        'dexterity value': 9,
+        'intel value': 0,
+        'health value':0}
+
+item62 = {'name':'Excellent Shield',
+        'strength value': 6,
+        'dexterity value': -5,
+        'intel value': 0,
+        'health value':29}
+
+item61 = {'name':'Excellent Short Sword',
+        'strength value': 26,
+        'dexterity value': 10,
+        'intel value': 0,
+        'health value':0}
+
+################
+#Advanced Items#
+################
+
+item60 = {'name':'Advanced Plate Helmet',
+        'strength value': 4,
+        'dexterity value': 7,
+        'intel value': 9,
+        'health value':30}
+
+item59 = {'name':'Advanced Plate Armor',
+        'strength value': 6,
+        'dexterity value': 8,
+        'intel value': 10,
+        'health value':35}
+
+
+item58 = {'name':'Advanced Mage Hood',
+        'strength value': -7,
+        'dexterity value': 5,
+        'intel value': 25,
+        'health value':8}
+
+
+item57 = {'name':'Advanced Mage Robes',
+        'strength value': -5,
+        'dexterity value': 6,
+        'intel value': 30,
+        'health value':8}
+
+
+item56 = {'name':'Advanced Leather Guise',
+        'strength value': 4,
+        'dexterity value': 5,
+        'intel value': 9,
+        'health value':25}
+
+
+item55 = {'name':'Advanced Leather Armor',
+        'strength value': 5,
+        'dexterity value': 6,
+        'intel value': 10,
+        'health value':30}
+
+
+item54 = {'name':'Advanced Dagger',
+        'strength value': 5,
+        'dexterity value': 17,
+        'intel value': 9,
+        'health value':-5}
+
+
+item53 = {'name':'Advanced Amplifiying Tome',
+        'strength value': -4,
+        'dexterity value': 4,
+        'intel value': 20,
+        'health value':0}
+
+item52 = {'name':'Advanced Staff',
+        'strength value': 5,
+        'dexterity value': 8,
+        'intel value': 20,
+        'health value':0}
+
+item51 = {'name':'Advanced Long Sword ',
+        'strength value': 20,
+        'dexterity value': 7,
+        'intel value': 0,
+        'health value':0}
+
+item50 = {'name':'Advanced Shield',
+        'strength value': 5,
+        'dexterity value': -4,
+        'intel value': 0,
+        'health value':24}
+
+item49 = {'name':'Advanced Short Sword',
+        'strength value': 17,
+        'dexterity value': 8,
+        'intel value': 0,
+        'health value':0}
+
+####################
+#Intermediate Items#
+####################
+
+item48 = {'name':'Intermediate Plate Helmet',
+        'strength value': 3,
+        'dexterity value':6,
+        'intel value': 8,
+        'health value':25}
+
+item47 = {'name':'Intermediate Plate Armor',
+        'strength value': 5,
+        'dexterity value': 7,
+        'intel value': 9,
+        'health value':30}
+
+item46 = {'name':'Intermediate Mage Hood',
+        'strength value': -5,
+        'dexterity value': 4,
+        'intel value': 20,
+        'health value':7}
+
+item45 = {'name':'Intermediate Mage Robes',
+        'strength value': -3,
+        'dexterity value': 5,
+        'intel value': 25,
+        'health value':9}
+
+item44 = {'name':'Intermediate Leather Guise',
+        'strength value': 3,
+        'dexterity value':4,
+        'intel value': 7,
+        'health value':20}
+
+item43 = {'name':'Intermediate Leather Armor',
+        'strength value': 4,
+        'dexterity value': 5,
+        'intel value': 8,
+        'health value':25}
+
+item42 = {'name':'Intermediate Dagger',
+        'strength value': 4,
+        'dexterity value': 13,
+        'intel value': 7,
+        'health value':-4}
+
+item41 = {'name':'IntermediateAmplifying Tome',
+        'strength value': -3,
+        'dexterity value': 3,
+        'intel value': 13,
+        'health value':0}
+
+item40 = {'name':'Intermediate Staff',
+        'strength value': 4,
+        'dexterity value': 6,
+        'intel value': 13,
+        'health value':0}
+
+item39 = {'name':'Intermediate Long Sword',
+        'strength value': 13,
+        'dexterity value': 5,
+        'intel value': 0,
+        'health value':0}
+
+item38 = {'name':'Intermediate Shield',
+        'strength value': 4,
+        'dexterity value':-4,
+        'intel value': 0,
+        'health value':19}
+
+item37 = {'name':'Intermediate Short Sword',
+        'strength value': 10,
+        'dexterity value': 6,
+        'intel value': 0,
+        'health value':0}
+
+print (item45['name'])
+
+
+
+
+
