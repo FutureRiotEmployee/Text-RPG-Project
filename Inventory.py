9a787b7a64f03ee77d8d5eaaec969bab767785af
DABABAYAGA <61705300+DABABAYAGA@users.noreply.github.com>
2020-03-12 12:05:27 -0400

    Add files via upload

---
+++ Inventory.py
@@ -0,0 +1,28 @@
+import random
+
+item1 = {'name':'Old Short Sword',
+        'strength value': 2,
+        'dexterity value': 0,
+        'intel value': 0,
+        'health value':0}
+
+
+
+item2 = {'name':'old sheild',
+        'strength value': 1,
+        'dexterity value': -2,
+        'intel value': 0,
+        'health value':7}
+
+
+
+item3 = {'name':'old long sword',
+        'strength value': 5,
+        'dexterity value': 0,
+        'intel value': 0,
+        'health value':0}
+
+
+print(item1)
+
+
