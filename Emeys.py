75f0ce31ee6ff7125aeac1380c9630d998291a48
DABABAYAGA <61705300+DABABAYAGA@users.noreply.github.com>
2020-03-12 12:08:05 -0400

    Add files via upload

---
+++ Emeys.py
@@ -0,0 +1,73 @@
+faf = {'name': 'Fafnir, Sovereign of Destruction',
+       'lvl': 47,
+       'xp': 0,
+       'lvlNext': 25,
+       'reward': 6000,
+       'stats':{'strE': 23,
+                'dexE': 14,
+                'intE': 17,
+                'hpE': 1000,
+                'maxhpE':1000,
+                'atkE':[5,8]}}
+
+
+lav = {'name': 'Lava Worm',
+       'lvl': 46,
+       'xp': 0,
+       'lvlNext': 25,
+       'reward': 5007,
+       'stats':{'strE': 14,
+                'dexE': 10,
+                'intE': 12,
+                'hpE': 687,
+                'maxhpE':687,
+                'atkE':[5,8]}}
+yoDr = {'name': 'Young Dragon',
+       'lvl': 45,
+       'xp': 0,
+       'lvlNext': 25,
+       'reward': 4203,
+       'stats':{'strE': 18,
+                'dexE': 9,
+                'intE': 6,
+                'hpE': 535,
+                'maxhpE':535,
+                'atkE':[5,8]}}
+
+
+ptd = {'name': 'Pteradon',
+       'lvl': 44,
+       'xp': 0,
+       'lvlNext': 25,
+       'reward': 3875,
+       'stats':{'strE': 16,
+                'dexE': 2,
+                'intE': 2,
+                'hpE': 425,
+                'maxhpE':425,
+                'atkE':[5,8]}}
+
+cme = {'name': 'Chimera',
+       'lvl': 43,
+       'xp': 0,
+       'lvlNext': 25,
+       'reward': 3098,
+       'stats':{'strE': 10,
+                'dexE': 12,
+                'intE': 11,
+                'hpE': 355,
+                'maxhpE':355,
+                'atkE':[5,8]}}
+
+fin = {'name': 'FireNewt',
+       'lvl': 42,
+       'xp': 0,
+       'lvlNext': 25,
+       'reward': 2678,
+       'stats':{'strE': 8,
+                'dexE': 9,
+                'intE': 13,
+                'hpE': 345,
+                'maxhpE':345,
+                'atkE':[5,8]}}
+
